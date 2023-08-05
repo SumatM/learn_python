c5a26d910d143ae49ab4e8526cc7125130cb2f90
Sumat Mallick <115460605+SumatM@users.noreply.github.com>
2023-08-06 00:03:10 +0530

    updated

---
+++ spritn-1/day6/set3/problem2.py
@@ -0,0 +1,18 @@
+#  **Dictionary Manipulation**: Create a dictionary with keys as names and values as ages. Write functions to add a new name-age pair, update the age of a name, and delete a name from the dictionary.
+#     - *Input*: Add "John": 25, Update "John": 26, Delete "John"
+#     - *Output*: "{}, {'John': 26}, {}"
+
+
+dictionary = {}
+
+print(dictionary)
+
+dictionary["john"] = 25;
+
+dictionary["john"] = 26;
+
+print(dictionary)
+
+del dictionary['john']
+
+print(dictionary)
